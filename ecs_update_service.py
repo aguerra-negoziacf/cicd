b777b35e10293963932577b4145d5478498a6b63
Miguel Baquero <miguel.baquero@data-tropics.com>
2021-11-12 21:41:12 +0000

    lint ecs_update_service.py

--- ecs_update_service.py
+++ ecs_update_service.py
@@ -1,4 +1,6 @@
-import boto3, os, json
+import boto3
+import os
+import json
 
 taskDefinitionName = os.getenv('TASK_DEFINITION_NAME')
 clusterName        = os.getenv('CLUSTER_NAME')
@@ -10,29 +12,30 @@ roleArn            = os.getenv('ROLE_ARN')
 sessionName        = os.getenv('SESSION_NAME', 'ecsDeploy')
 envNvars           = os.getenv('ENV_VARS')
 
-def updateTaskDefinition(client, taskDefinitionName, newEcrImage, clusterName, serviceName):
+
+def updateTaskDefinition(client, taskDefinitionName, newEcrImage,
+                         clusterName, serviceName):
     taskDefResponse    = client.describe_task_definition(
         taskDefinition = taskDefinitionName
     )
     for container in taskDefResponse['taskDefinition']['containerDefinitions']:
         container['image'] = newEcrImage
-        if envNvars != None:
+        if envNvars is not None:
             data = json.loads(envNvars)
             for environment in data:
                 container[environment] = data['environment']
-                
     taskDefRegisterResponse = client.register_task_definition(
         family               = taskDefinitionName,
-        containerDefinitions = taskDefResponse['taskDefinition']['containerDefinitions']
+        containerDefinitions = taskDefResponse['taskDefinition']
+                                              ['containerDefinitions']
     )
-    
     revision = (taskDefRegisterResponse['taskDefinition']['revision'])
-    
     client.update_service(
         cluster        = clusterName,
         service        = serviceName,
         taskDefinition = taskDefinitionName + ':' + str(revision)
     )
+
 
 def waitForServiceUpdate(client, clusterName, serviceName):
     waiter = client.get_waiter('services_stable')
@@ -45,22 +48,24 @@ def waitForServiceUpdate(client, clusterName, serviceName):
         }
     )
 
+
 def assumeRole(roleArn, sessionName):
     client = boto3.client('sts')
-    
     response = client.assume_role(RoleArn=roleArn, RoleSessionName=sessionName)
     session = boto3.Session(
         aws_access_key_id     = response['Credentials']['AccessKeyId'],
         aws_secret_access_key = response['Credentials']['SecretAccessKey'],
         aws_session_token     = response['Credentials']['SessionToken'])
-    
     client = session.client('ecs')
     return client
 
+
 def main():
     client = assumeRole(roleArn, serviceName)
-    updateTaskDefinition(client, taskDefinitionName, newEcrImage, clusterName, serviceName)
+    updateTaskDefinition(client, taskDefinitionName, newEcrImage,
+                         clusterName, serviceName)
     waitForServiceUpdate(client, clusterName, serviceName)
+
 
 if __name__ == "__main__":
     main()
